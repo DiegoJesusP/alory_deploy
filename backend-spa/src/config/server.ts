be7fb0df81f3c5b15bed06af5ce1cd66c645de9a
DiegoJesusP <spartfomi@outlook.com>
2026-04-08 16:33:25 -0600

    fix server env loading

--- backend-spa/src/config/server.ts
+++ backend-spa/src/config/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+
 import { globalLimiter } from '../security/rate-limit'
 import authRoutes from '../modules/auth/auth.routes'
 import userRoutes from '../modules/users/user.routes'
@@ -10,22 +12,24 @@ import serviceRoutes from '../modules/services/service.routes'
 import productRoutes from '../modules/products/product.routes'
 import appointmentRoutes from '../modules/appointments/appointment.routes'
 
-process.loadEnvFile()
+dotenv.config()
 
 const app = express()
 
 app.set('port', process.env.PORT || 3001)
 
 app.use(helmet())
+
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.APP_URL || 'http://localhost:5173',
     credentials: true,
 }))
+
 app.use(cookieParser())
 app.use(globalLimiter)
 app.use(express.json({ limit: '50mb' }))
 
-app.get('/', (request: express.Request, response: express.Response) => {
+app.get('/', (_request: express.Request, response: express.Response) => {
     response.send('Spa Management API')
 })
 
